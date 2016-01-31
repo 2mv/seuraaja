a66d9f46cbfe6d8be1e7ffd20739c4e3077abf04
Teemu Pääkkönen <teemu@flockler.com>
2016-01-31 23:31:59 +0200

    create converter for reading pdf file to xml

---
+++ converter.py
@@ -0,0 +1,22 @@
+from cStringIO import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import XMLConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+class Converter:
+
+    @staticmethod
+    def to_xml(infile):
+        output = StringIO()
+        manager = PDFResourceManager()
+        converter = XMLConverter(manager, output, laparams=LAParams())
+        interpreter = PDFPageInterpreter(manager, converter)
+
+        for page in PDFPage.get_pages(infile):
+            interpreter.process_page(page)
+        converter.close()
+        xml = output.getvalue()
+        output.close
+        return xml
